245386f0da094b18b6f3224ad255dae4cd6623be
Chris Jacobs <apexearth@gmail.com>
2023-09-18 09:41:39 -0700

    add & run prettier scripts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
 import { run } from './processor'
-
-import * as oethProcessor from './processors/oeth'
-import * as vaultProcessor from './processors/vault'
+import * as dripperProcessor from './processors/dripper'
 import * as fraxStakingProcessor from './processors/frax-staking'
 import * as morphoAaveProcessor from './processors/morpho-aave'
-import * as dripperProcessor from './processors/dripper'
+import * as oethProcessor from './processors/oeth'
+import * as vaultProcessor from './processors/vault'
 
 // The idea is that these processors have zero dependencies on one another and can be processed asynchronously.
 run([
