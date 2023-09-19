526de24ba461b5106ee50209bf312818c6dd7404
Chris Jacobs <apexearth@gmail.com>
2023-09-19 10:36:50 -0700

    accurate curve lp numbers

---
+++ src/model/generated/curveLp.model.ts
@@ -0,0 +1,42 @@
+import {Entity as Entity_, Column as Column_, PrimaryColumn as PrimaryColumn_, Index as Index_} from "typeorm"
+import * as marshal from "./marshal"
+
+@Entity_()
+export class CurveLP {
+    constructor(props?: Partial<CurveLP>) {
+        Object.assign(this, props)
+    }
+
+    @PrimaryColumn_()
+    id!: string
+
+    @Index_()
+    @Column_("timestamp with time zone", {nullable: false})
+    timestamp!: Date
+
+    @Index_()
+    @Column_("int4", {nullable: false})
+    blockNumber!: number
+
+    @Index_()
+    @Column_("text", {nullable: false})
+    txHash!: string
+
+    @Column_("numeric", {transformer: marshal.bigintTransformer, nullable: false})
+    totalSupply!: bigint
+
+    @Column_("numeric", {transformer: marshal.bigintTransformer, nullable: false})
+    eth!: bigint
+
+    @Column_("numeric", {transformer: marshal.bigintTransformer, nullable: false})
+    oeth!: bigint
+
+    @Column_("numeric", {transformer: marshal.bigintTransformer, nullable: false})
+    totalSupplyOwned!: bigint
+
+    @Column_("numeric", {transformer: marshal.bigintTransformer, nullable: false})
+    ethOwned!: bigint
+
+    @Column_("numeric", {transformer: marshal.bigintTransformer, nullable: false})
+    oethOwned!: bigint
+}
