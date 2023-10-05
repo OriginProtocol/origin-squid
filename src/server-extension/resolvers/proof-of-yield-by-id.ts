ed128fb47e43f5f7a8737e8871d3c169d9a05ca6
Nick Poulden <nick@poulden.com>
2023-10-05 15:58:08 -0600

    Proof of yield

--- src/server-extension/resolvers/proof-of-yield-by-id.ts
+++ src/server-extension/resolvers/proof-of-yield-by-id.ts
@@ -26,7 +26,19 @@ export class ProofOfYieldByIdResult {
   yield!: bigint
 
   @Field(() => BigInt, { nullable: false })
+  fees!: bigint
+
+  @Field(() => BigInt, { nullable: false })
+  totalSupply!: bigint
+
+  @Field(() => BigInt, { nullable: false })
   rebasingSupply!: bigint
+
+  @Field(() => BigInt, { nullable: false })
+  nonRebasingSupply!: bigint
+
+  @Field(() => BigInt, { nullable: false })
+  amoSupply!: bigint
 
   constructor(props: Partial<ProofOfYieldByIdResult>) {
     Object.assign(this, props)
@@ -47,50 +59,46 @@ export class ProofOfYieldByIdResolver {
 
     const manager = await this.tx()
 
-    const sql = `WITH
-    oeth_aggregated AS (
-        SELECT
-            DATE(timestamp) AS date,
-            MAX(rebasing_supply) AS max_rebasing_supply
-        FROM
-            oeth
-        GROUP BY
-            DATE(timestamp)
-    ),
-
-    apy_aggregated AS (
-        SELECT
-            DATE(timestamp) AS date,
-            MAX(apy7_day_avg) AS max_apy7_day_avg
-        FROM
-            apy
-        GROUP BY
-            DATE(timestamp)
-    ),
-
-    rebase_aggregated AS (
-        SELECT
-            DATE(timestamp) AS date,
-            SUM(yield - fee) AS total_yield
-        FROM
-            rebase
-        GROUP BY
-            DATE(timestamp)
-    )
-
-    SELECT
-        TO_CHAR(o.date, 'YYYY-MM-DD') as id,
-        o.date as timestamp,
-        a.max_apy7_day_avg as apy,
-        o.max_rebasing_supply as "rebasingSupply",
-        r.total_yield as yield
+    const sql = `SELECT
+        $1 as timestamp,
+        oeth_data.rebasing_supply AS "rebasingSupply",
+        oeth_data.total_supply AS "totalSupply",
+        oeth_data.non_rebasing_supply AS "nonRebasingSupply",
+        apy_data.apy7_day_avg as apy,
+        curve_data.oeth_owned AS "amoSupply",
+        rebase_data.total_yield as yield,
+        rebase_data.total_fees as fees
     FROM
-        oeth_aggregated o
-    JOIN
-        apy_aggregated a ON o.date = a.date
-    LEFT JOIN
-        rebase_aggregated r ON o.date = r.date
-    WHERE o.date = $1;`
+        (SELECT 1) AS dummy -- A dummy table to ensure we always get a row
+    LEFT JOIN LATERAL (
+        SELECT oeth.rebasing_supply, oeth.total_supply, oeth.non_rebasing_supply
+        FROM oeth
+        WHERE DATE(oeth.timestamp) <= $1
+        ORDER BY oeth.timestamp DESC
+        LIMIT 1
+    ) AS oeth_data ON TRUE
+    LEFT JOIN LATERAL (
+        SELECT apy.apy7_day_avg
+        FROM apy
+        WHERE DATE(apy.timestamp) <= $1
+        ORDER BY apy.timestamp DESC
+        LIMIT 1
+    ) AS apy_data ON TRUE
+    LEFT JOIN LATERAL (
+        SELECT c.oeth_owned
+        FROM curve_lp c
+        WHERE DATE(c.timestamp) <= $1
+        ORDER BY c.timestamp DESC
+        LIMIT 1
+    ) AS curve_data ON TRUE
+    LEFT JOIN (
+        SELECT
+            DATE(timestamp) AS date,
+            SUM(yield - fee) AS total_yield,
+            SUM(fee) AS total_fees
+        FROM rebase
+        GROUP BY DATE(timestamp)
+    ) AS rebase_data ON rebase_data.date = $1;`
 
     const result = await manager.getRepository(OETH).query(sql, [id])
     return result?.[0]
