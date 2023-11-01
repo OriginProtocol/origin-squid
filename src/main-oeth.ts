40e826245a7cce95c72d0afb66715a7c1f2e0fa3
Chris Jacobs <apexearth@gmail.com>
2023-11-01 10:43:57 -0700

    track eth usd exchange rates more frequently
    (cherry picked from commit 4277024a3d65b7b97145ef6270bc600e1cb456a2)

--- src/main-oeth.ts
+++ src/main-oeth.ts
@@ -3,13 +3,14 @@ import * as oeth from './oeth/processors'
 import * as balancerMetaPoolStrategy from './oeth/processors/balancer-meta-pool'
 import * as curveLp from './oeth/processors/curve-lp'
 import * as dripper from './oeth/processors/dripper'
+import * as exchangeRates from './oeth/processors/exchange-rates'
 import * as fraxStaking from './oeth/processors/frax-staking'
 import * as morphoAave from './oeth/processors/morpho-aave'
 import * as strategies from './oeth/processors/strategies'
 import * as vault from './oeth/processors/vault'
 import * as validateOeth from './oeth/validators/validate-oeth'
 import { run } from './processor'
-import * as exchangeRates from './shared/post-processors/exchange-rates'
+import * as exchangeRatesPostProcessor from './shared/post-processors/exchange-rates'
 
 run({
   stateSchema: 'oeth-processor',
@@ -22,7 +23,8 @@ run({
     curveLp,
     balancerMetaPoolStrategy,
     strategies,
+    exchangeRates,
   ],
-  postProcessors: [exchangeRates, dailyStats],
+  postProcessors: [exchangeRatesPostProcessor, dailyStats],
   validators: [validateOeth],
 })
