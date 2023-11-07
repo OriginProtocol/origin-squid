ea1fb499aa937107e95aa09224dac2b266bc41b9
Chris Jacobs <apexearth@gmail.com>
2023-11-07 15:27:45 -0800

    post merge fixes

--- src/ousd/processors/ousd/ousd.ts
+++ src/ousd/processors/ousd/ousd.ts
@@ -13,8 +13,8 @@ import {
 import { OUSD_ADDRESS, OUSD_VAULT_ADDRESS } from '../../../utils/addresses'
 
 // export const from = 10884563 // https://etherscan.io/tx/0x9141921f5ebf072e58c00fe56332b6bee0c02f0ae4f54c42999b8a3a88662681
-// export const from = 11585978 // OUSDReset - Has issues with archive queries. :(
-export const from = 13533937 // https://etherscan.io/tx/0xc9b6fc6a4fad18dad197ff7d0636f74bf066671d75656849a1c45122e00d54cf
+export const from = 11585978 // OUSDReset - Has issues with archive queries. :(
+// export const from = 13533937 // https://etherscan.io/tx/0xc9b6fc6a4fad18dad197ff7d0636f74bf066671d75656849a1c45122e00d54cf
 
 export const setup = createOTokenSetup({
   address: OUSD_ADDRESS,
