6fb371ae0d050fca2f076c308dfcd91c20ceb23e
Nick Poulden <nick@poulden.com>
2023-11-16 15:32:27 -0700

    Address constants

--- src/utils/addresses.ts
+++ src/utils/addresses.ts
@@ -4,24 +4,17 @@ export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
 export const ETH_ADDRESS = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'
 
 export const OUSD_ADDRESS = '0x2a8e1e676ec238d8a992307b495b45b3feaa5e86'
-<<<<<<< Updated upstream
-=======
 export const DAI_ADDRESS = '0x6b175474e89094c44da98b954eedeac495271d0f'
 export const USDT_ADDRESS = '0xdac17f958d2ee523a2206206994597c13d831ec7'
 export const USDC_ADDRESS = '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'
 export const CURVE_EXCHANGE_ADDRESS = `0x99a58482bd75cbab83b27ec03ca68ff489b5788f`
->>>>>>> Stashed changes
 export const OUSD_VAULT_ADDRESS = '0xe75d77b1865ae93c7eaa3040b038d7aa7bc02f70'
 
 export const OETH_ADDRESS = '0x856c4efb76c1d1ae02e20ceb03a2a6a08b0b8dc3'
 export const OETH_VAULT_ADDRESS = '0x39254033945aa2e4809cc2977e7087bee48bd7ab'
-<<<<<<< Updated upstream
-=======
 export const OETH_HARVESTER_ADDRESS =
   '0x0d017afa83eace9f10a8ec5b6e13941664a6785c'
-export const OETH_DRIPPER_ADDRESS = '0xc0f42f73b8f01849a2dd99753524d4ba14317eb3'
 export const OETH_ZAPPER_ADDRESS = '0x9858e47bcbbe6fbac040519b02d7cd4b2c470c66'
->>>>>>> Stashed changes
 
 export const WETH_ADDRESS = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
 export const STETH_ADDRESS = '0xae7ab96520de3a18e5e111b5eaab095312d7fe84'
@@ -57,15 +50,10 @@ export const OETH_DRIPPER_ADDRESS = '0xc0f42f73b8f01849a2dd99753524d4ba14317eb3'
 export const OETH_STRATEGY_BALANCER_ADDRESS =
   '0x49109629ac1deb03f2e9b2fe2ac4a623e0e7dfdc'
 
-
 export const BALANCER_VAULT = '0xba12222222228d8ba445958a75a0704d566bf2c8'
 
+export const GOVERNANCE_ADDRESS = '0x3cdd07c16614059e66344a7b579dab4f9516c0b6'
 
-export const GOVERNANCE_ADDRESS = 
-  '0x3cdd07c16614059e66344a7b579dab4f9516c0b6'
+export const OGV_ADDRESS = '0x9c354503c38481a7a7a51629142963f98ecc12d0'
 
-export const OGV_ADDRESS = 
-  '0x9c354503c38481a7a7a51629142963f98ecc12d0'
-
-export const VEOGV_ADDRESS = 
-  '0x0c4576ca1c365868e162554af8e385dc3e7c66d9'
+export const VEOGV_ADDRESS = '0x0c4576ca1c365868e162554af8e385dc3e7c66d9'
