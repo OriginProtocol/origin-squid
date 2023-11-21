18804add4f3bf82bf6532a695d5a8f76f936aaf2
Chris Jacobs <apexearth@gmail.com>
2023-11-21 09:03:52 -0800

    remake migration file

--- db/migrations/1700586218500-Data.js
+++ db/migrations/1700586218500-Data.js
@@ -1,5 +1,5 @@
-module.exports = class Data1700586142636 {
-    name = 'Data1700586142636'
+module.exports = class Data1700586218500 {
+    name = 'Data1700586218500'
 
     async up(db) {
         await db.query(`CREATE TABLE "exchange_rate" ("id" character varying NOT NULL, "timestamp" TIMESTAMP WITH TIME ZONE NOT NULL, "block_number" integer NOT NULL, "pair" text NOT NULL, "base" text NOT NULL, "quote" text NOT NULL, "rate" numeric NOT NULL, CONSTRAINT "PK_5c5d27d2b900ef6cdeef0398472" PRIMARY KEY ("id"))`)
