081ee0ffe4881e11d844fdbebd15b25dbca3a4e9
Chris Jacobs <apexearth@gmail.com>
2023-12-07 10:23:47 -0800

    retrieve compound USDT, USDC, and DAI

--- src/main-other.ts
+++ src/main-other.ts
@@ -1,13 +1,12 @@
 import { run } from './processor'
 import * as exchangeRates from './shared/post-processors/exchange-rates'
-import * as aaveCompound from './shared/processors/aave-compound'
 import * as balancer from './shared/processors/balancer'
 import * as curve from './shared/processors/curve'
 import { erc20s } from './shared/processors/erc20s'
 
 export const processor = {
   stateSchema: 'other-processor',
-  processors: [aaveCompound, balancer, curve, ...erc20s],
+  processors: [balancer, curve, ...erc20s],
   postProcessors: [exchangeRates],
   validators: [],
 }
--- src/shared/processors/erc20s.ts
+++ src/shared/processors/erc20s.ts
@@ -3,7 +3,38 @@ import { OETH_ADDRESS } from '../../utils/addresses'
 import { logFilter } from '../../utils/logFilter'
 import { createERC20Tracker } from '../processor-templates/erc20'
 
+const tracks: Parameters<typeof createERC20Tracker>[0][] = [
+  {
+    from: 11362821,
+    address: '0xdac17f958d2ee523a2206206994597c13d831ec7', // USDT
+    accountFilter: [
+      '0x3ed3b47dd13ec9a98b44e6204a523e766b225811', // aUSDT
+      '0xf650c3d88d12db855b8bf7d11be6c55a4e07dcc9', // aUSDT
+    ],
+    intervalTracking: true,
+  },
+  {
+    from: 11367200,
+    address: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48', // USDC
+    accountFilter: [
+      '0xbcca60bb61934080951369a648fb03df4f96263c', // aUSDC
+      '0x39aa39c021dfbae8fac545936693ac917d5e7563', // cUSDC
+    ],
+    intervalTracking: true,
+  },
+  {
+    from: 11367184,
+    address: '0x6b175474e89094c44da98b954eedeac495271d0f', // DAI
+    accountFilter: [
+      '0x028171bca77440897b824ca71d1c56cac55b68a3', // aDAI
+      '0x5d3a536e4d6dbd6114cc1ead35777bab948e3643', // cDAI
+    ],
+    intervalTracking: true,
+  },
+]
+
 export const erc20s = [
+  ...tracks.map(createERC20Tracker),
   createERC20Tracker({
     from: 16935276,
     address: OETH_ADDRESS,
