0275f779dce27dcac76c96114165b26e1c045da2
Chris Jacobs <apexearth@gmail.com>
2024-02-04 14:09:18 -0800

    order balances ascending

--- src/lrt/state.ts
+++ src/lrt/state.ts
@@ -32,6 +32,7 @@ export const getBalanceDataForRecipient = async (
         balance: MoreThan(0n),
       },
     ],
+    order: { id: 'asc' },
     relations: { recipient: true },
   })
   const state = useLrtState()
