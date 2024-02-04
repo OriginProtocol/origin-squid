97f00985de89b01985d547777b76e2f29e9b003e
Chris Jacobs <apexearth@gmail.com>
2024-02-03 22:19:27 -0800

    update resolver

--- src/server-extension/lrt.ts
+++ src/server-extension/lrt.ts
@@ -1,15 +1,30 @@
-import { Arg, Query, Resolver } from 'type-graphql'
+import { Arg, Field, ObjectType, Query, Resolver } from 'type-graphql'
 import type { EntityManager } from 'typeorm'
 
 import { calculateRecipientsPoints } from '../lrt/calculation'
 import { LRTPointRecipient } from '../model'
 
+@ObjectType()
+export class LRTPointRecipientStats {
+  @Field(() => BigInt, { nullable: false })
+  points!: bigint
+
+  @Field(() => BigInt, { nullable: false })
+  elPoints!: bigint
+
+  constructor(props: Partial<LRTPointRecipientStats>) {
+    Object.assign(this, props)
+  }
+}
+
 @Resolver()
 export class LRTResolver {
   constructor(private tx: () => Promise<EntityManager>) {}
 
-  @Query(() => BigInt)
-  async lrtDepositorPoints(@Arg('address') address: string): Promise<bigint> {
+  @Query(() => LRTPointRecipientStats)
+  async lrtPointRecipientStats(
+    @Arg('address') address: string,
+  ): Promise<{ points: bigint; elPoints: bigint }> {
     const manager = await this.tx()
     const recipients = await manager.getRepository(LRTPointRecipient).find({
       where: { id: address.toLowerCase() },
@@ -17,6 +32,9 @@ export class LRTResolver {
         balanceData: true,
       },
     })
-    return calculateRecipientsPoints(Date.now(), recipients)
+    return {
+      points: calculateRecipientsPoints(Date.now(), recipients),
+      elPoints: recipients[0].elPoints,
+    }
   }
 }
