c6a6de72deb6ae006a519634e556e079ce7629dc
Nick Poulden <nick@poulden.com>
2024-05-28 09:34:50 -0600

    Include old list

--- src/server-extension/ogn-stats.ts
+++ src/server-extension/ogn-stats.ts
@@ -69,6 +69,15 @@ export class OGNStatsResolver {
 
 async function getCirculatingSupply(): Promise<number> {
   const excludeBalance = [
+    // '0xe011fa2a6df98c69383457d87a056ed0103aa352', // foundation_reserve
+    // '0xcaa5ef7abc36d5e5a3e4d7930dcff3226617a167', // team_dist
+    // '0x2eae0cae2323167abf78462e0c0686865c67a655', // new_team_dist
+    // '0x3da5045699802ea1fcc60130dedea67139c5b8c0', // investor_dist
+    // '0xfe730b3cf80ca7b31905f70241f7c786baf443e3', // new_investor_dist
+    // '0x1a34e5b97d684b124e32bd3b7dc82736c216976b', // dist_staging
+    // '0xbc0722eb6e8ba0217aeea5694fe4f214d2e53017', // partnerships
+    // '0x2d00c3c132a0567bbbb45ffcfd8c6543e08ff626', // ecosystem_growth
+
     '0xbe2ab3d3d8f6a32b96414ebbd865dbd276d3d899', // Treasury
     '0xa2cc2eae69cbf04a3d5660bc3e689b035324fc3f', // Custodian #1
     '0x8ac3b96d118288427055ae7f62e407fC7c482F57', // Custodian #2
