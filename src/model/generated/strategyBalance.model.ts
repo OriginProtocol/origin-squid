c1317c7d7272ed7d7bd59b50626d281c23fd429b
Chris Jacobs <apexearth@gmail.com>
2024-07-19 16:34:30 -0700

    native staking balance and earning

--- src/model/generated/strategyBalance.model.ts
+++ src/model/generated/strategyBalance.model.ts
@@ -1,4 +1,4 @@
-import {Entity as Entity_, Column as Column_, PrimaryColumn as PrimaryColumn_, DateTimeColumn as DateTimeColumn_, Index as Index_, IntColumn as IntColumn_, StringColumn as StringColumn_, BigIntColumn as BigIntColumn_} from "@subsquid/typeorm-store"
+import {Entity as Entity_, Column as Column_, PrimaryColumn as PrimaryColumn_, IntColumn as IntColumn_, Index as Index_, DateTimeColumn as DateTimeColumn_, StringColumn as StringColumn_, BigIntColumn as BigIntColumn_} from "@subsquid/typeorm-store"
 
 @Entity_()
 export class StrategyBalance {
@@ -7,10 +7,14 @@ export class StrategyBalance {
     }
 
     /**
-     * Format: 'strategy:asset:blockNumber'
+     * Format: 'chainId:strategy:asset:blockNumber'
      */
     @PrimaryColumn_()
     id!: string
+
+    @Index_()
+    @IntColumn_({nullable: false})
+    chainId!: number
 
     @Index_()
     @DateTimeColumn_({nullable: false})
