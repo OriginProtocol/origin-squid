f3a58528bddcb543766ac8714838cc67b8991f56
Chris Jacobs <apexearth@gmail.com>
2024-10-09 15:17:11 -0700

    feat: arm - throttle multicalls to 50 to avoid overloading tenderly RPC

--- src/utils/multicall.ts
+++ src/utils/multicall.ts
@@ -3,18 +3,9 @@ import { arbitrum, mainnet } from 'viem/chains'
 
 import { Multicall } from '@abi/multicall'
 import { Block, Context } from '@processor'
-import {
-  type AbiFunction,
-  ContractBase,
-  type FunctionArguments,
-  type FunctionReturn,
-  fun,
-} from '@subsquid/evm-abi'
+import { type AbiFunction, type FunctionArguments } from '@subsquid/evm-abi'
 
-const MULTICALL_CONTRACTS: Record<
-  number,
-  undefined | { from: number; address: string }
-> = {
+const MULTICALL_CONTRACTS: Record<number, undefined | { from: number; address: string }> = {
   [mainnet.id]: {
     from: 12336033,
     address: '0x5ba1e12693dc8f9c48aad8770482f4739beed696',
@@ -30,19 +21,25 @@ export const multicall = async <Function extends AbiFunction<any, any>>(
   func: Function,
   address: string,
   calls: FunctionArguments<Function>[],
+  throttle = 50,
 ) => {
-  const multicallContract = MULTICALL_CONTRACTS[ctx.chain.id]
-  if (multicallContract && header.height >= multicallContract.from) {
-    const multicall = new Multicall(ctx, header, multicallContract.address)
-    return multicall.aggregate(func, address, calls)
+  const results = [] as ReturnType<Function['decodeResult']>[]
+  const pendingCalls = [...calls]
+  while (pendingCalls.length > 0) {
+    const callsToMake = pendingCalls.splice(0, throttle)
+    const multicallContract = MULTICALL_CONTRACTS[ctx.chain.id]
+    if (multicallContract && header.height >= multicallContract.from) {
+      const multicall = new Multicall(ctx, header, multicallContract.address)
+      const response = await multicall.aggregate(func, address, callsToMake)
+      results.push(...response)
+    } else {
+      const batchCalls = callsToMake.map((fnParams) => ({
+        method: 'eth_call',
+        params: [{ to: address, data: func.encode(fnParams) }, toHex(header.height)],
+      }))
+      const response = await ctx._chain.client.batchCall(batchCalls)
+      results.push(...response.map((r) => func.decodeResult(r)))
+    }
   }
-  const batchCalls = calls.map((fnParams) => ({
-    method: 'eth_call',
-    params: [
-      { to: address, data: func.encode(fnParams) },
-      toHex(header.height),
-    ],
-  }))
-  const results = await ctx._chain.client.batchCall(batchCalls)
-  return results.map((r) => func.decodeResult(r))
+  return results
 }
