cafb8fa5f919e741d644aec7d68d5d58a81ee7b2
Chris Jacobs <apexearth@gmail.com>
2024-10-10 20:53:45 -0700

    feat: arm - update fork blocks and rpc

--- src/main-mainnet.ts
+++ src/main-mainnet.ts
@@ -41,7 +41,7 @@ export const processor = {
     createFRRSProcessor({ from: 19917521, address: OGN_REWARDS_SOURCE_ADDRESS }),
     ...createOriginARMProcessors({
       name: 'origin-arm',
-      from: 20919108,
+      from: 20933088,
       armAddress: '0x85b78aca6deae198fbf201c82daf6ca21942acc6',
       liquidityProviderControllerAddress: '0x3817e247023B4f489352758397040b1fd33b300a',
     }),
