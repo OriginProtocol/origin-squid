fc9c5a96b967aa01bcecab8b3e13117f2604c5e7
Chris Jacobs <apexearth@gmail.com>
2024-10-23 11:40:05 -0700

    disable histories validation due to id change

--- src/oeth/validators/validate-oeth/validate-oeth.ts
+++ src/oeth/validators/validate-oeth/validate-oeth.ts
@@ -8,7 +8,6 @@ import {
   OToken,
   OTokenAPY,
   OTokenDailyStat,
-  OTokenHistory,
   OTokenRebase,
   StrategyBalance,
 } from '@model'
@@ -26,7 +25,7 @@ export const process = async (ctx: Context) => {
   for (const block of ctx.blocks) {
     await validateExpectations(ctx, block, OToken, firstBlock, entities.oeth_oTokens)
     await validateExpectations(ctx, block, OTokenAPY, firstBlock, entities.oeth_oTokenApies)
-    await validateExpectations(ctx, block, OTokenHistory, firstBlock, entities.oeth_oTokenHistories)
+    // await validateExpectations(ctx, block, OTokenHistory, firstBlock, entities.oeth_oTokenHistories)
     await validateExpectations(ctx, block, OTokenRebase, firstBlock, entities.oeth_oTokenRebases)
     await validateExpectations(ctx, block, OTokenDailyStat, firstBlock, entities.oeth_oTokenDailyStats)
     await validateExpectations(ctx, block, ERC20Balance, firstBlock, entities.oeth_erc20Balances)
--- src/ousd/validators/validate-ousd/validate-ousd.ts
+++ src/ousd/validators/validate-ousd/validate-ousd.ts
@@ -1,4 +1,4 @@
-import { ERC20Balance, OToken, OTokenAPY, OTokenDailyStat, OTokenHistory, OTokenRebase } from '@model'
+import { ERC20Balance, OToken, OTokenAPY, OTokenDailyStat, OTokenRebase } from '@model'
 import { Context } from '@processor'
 import { env } from '@utils/env'
 import { entities } from '@validation/entities'
@@ -13,7 +13,7 @@ export const process = async (ctx: Context) => {
   for (const block of ctx.blocks) {
     await validateExpectations(ctx, block, OToken, firstBlock, entities.ousd_oTokens)
     await validateExpectations(ctx, block, OTokenAPY, firstBlock, entities.ousd_oTokenApies)
-    await validateExpectations(ctx, block, OTokenHistory, firstBlock, entities.ousd_oTokenHistories)
+    // await validateExpectations(ctx, block, OTokenHistory, firstBlock, entities.ousd_oTokenHistories)
     await validateExpectations(ctx, block, OTokenRebase, firstBlock, entities.ousd_oTokenRebases)
     await validateExpectations(ctx, block, OTokenDailyStat, firstBlock, entities.ousd_oTokenDailyStats)
     await validateExpectations(ctx, block, ERC20Balance, firstBlock, entities.ousd_erc20Balances)
