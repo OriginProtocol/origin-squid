cf64f1eb411bada41824b05eaf0c0a0d234271c3
Chris Jacobs <apexearth@gmail.com>
2024-12-18 20:52:47 -0800

    add script to log processing times

---
+++ scripts/check-processing-times.ts
@@ -0,0 +1,61 @@
+import fs from 'fs'
+
+const LIMIT = 1000
+
+const gql = (query: string) => query
+
+const executeQuery = async <T>(query: string): Promise<T> => {
+  const response = await fetch(`https://origin.squids.live/origin-squid@${process.argv[2]}/api/graphql`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ query }),
+  })
+  const text = await response.text()
+  try {
+    return JSON.parse(text)
+  } catch (err) {
+    console.log(text)
+    throw err
+  }
+}
+
+const main = async () => {
+  const result = await executeQuery<{
+    data: {
+      processingStatuses: {
+        id: string
+        startTimestamp: string
+        headTimestamp: string
+        blockNumber: string
+        timestamp: string
+      }[]
+    }
+  }>(
+    gql(`
+    query MyQuery {
+      processingStatuses(limit: 10) {
+        id
+        startTimestamp
+        headTimestamp
+        blockNumber
+        timestamp
+      }
+    }
+  `),
+  )
+
+  let output = `Processing Times\n`
+  output += '==============================\n'
+  output += `Version: ${process.argv[2]}\nDate: ${new Date().toISOString()}\n`
+  output += '==============================\n'
+  for (const status of result.data.processingStatuses) {
+    const minutes = (Date.parse(status.headTimestamp) - Date.parse(status.startTimestamp)) / (1000 * 60)
+    output += `${status.id}: ${minutes.toFixed(2)} minutes\n`
+  }
+  console.log(output)
+  fs.writeFileSync(`processing-times.log`, output)
+}
+
+main()
