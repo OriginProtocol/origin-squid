492e4323d1c4b1f1e3c7878365cd58f9ac74be6d
Chris Jacobs <apexearth@gmail.com>
2025-01-29 21:24:13 -0800

    fix attempt for stuck sonic processor

--- src/templates/otoken/otoken.ts
+++ src/templates/otoken/otoken.ts
@@ -1171,7 +1171,7 @@ export const createOTokenProcessor = (params: {
     }
     await ctx.store.upsert(result.apies)
     await Promise.all([
-      ctx.store.insert(result.otokens),
+      ctx.store.upsert(result.otokens), // TODO: Consider changing otoken ID to block number instead of timestamp.
       ctx.store.insert(result.wotokens),
       ctx.store.insert(result.assets),
       ctx.store.insert(result.history),
