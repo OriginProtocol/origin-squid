0da7cb78be5352b40b836b93a16cf0f1eddaab5a
Chris Jacobs <apexearth@gmail.com>
2025-02-10 19:31:03 -0800

    pool booster wip

---
+++ src/model/generated/poolBoosterCampaign.model.ts
@@ -0,0 +1,38 @@
+import {Entity as Entity_, Column as Column_, PrimaryColumn as PrimaryColumn_, IntColumn as IntColumn_, Index as Index_, StringColumn as StringColumn_, BigIntColumn as BigIntColumn_, BooleanColumn as BooleanColumn_} from "@subsquid/typeorm-store"
+
+@Entity_()
+export class PoolBoosterCampaign {
+    constructor(props?: Partial<PoolBoosterCampaign>) {
+        Object.assign(this, props)
+    }
+
+    @PrimaryColumn_()
+    id!: string
+
+    @Index_()
+    @IntColumn_({nullable: false})
+    chainId!: number
+
+    @Index_()
+    @StringColumn_({nullable: false})
+    address!: string
+
+    @Index_()
+    @StringColumn_({nullable: false})
+    gauge!: string
+
+    @StringColumn_({nullable: false})
+    rewardToken!: string
+
+    @BigIntColumn_({nullable: false})
+    maxRewardPerVote!: bigint
+
+    @BigIntColumn_({nullable: false})
+    totalRewardAmount!: bigint
+
+    @BigIntColumn_({nullable: false})
+    campaignId!: bigint
+
+    @BooleanColumn_({nullable: false})
+    closed!: boolean
+}
--- src/templates/otoken/curve-pool-booster.ts
+++ src/templates/otoken/curve-pool-booster.ts
@@ -1,9 +1,11 @@
 import * as curvePoolBoosterAbi from '@abi/otoken-curve-pool-booster'
-import { Block, Context, blockFrequencyUpdater, defineProcessor, logFilter } from '@originprotocol/squid-utils'
+import { PoolBoosterCampaign, PoolBoosterFeeCollected } from '@model'
+import { Context, blockFrequencyUpdater, defineProcessor, logFilter } from '@originprotocol/squid-utils'
 import { EvmBatchProcessor } from '@subsquid/evm-processor'
 
-export const createCurvePoolBoosterProcessor = (params: { address: string; otokenAddress: string; from: number }) => {
+export const createCurvePoolBoosterProcessor = (params: { otokenAddress: string; from: number }) => {
   const frequencyUpdate = blockFrequencyUpdater({ from: params.from })
+
   const bribeCreatedFilter = logFilter({
     topic0: [curvePoolBoosterAbi.events.FeeCollected.topic],
     range: { from: params.from },
@@ -12,45 +14,73 @@ export const createCurvePoolBoosterProcessor = (params: { address: string; otoke
     topic0: [curvePoolBoosterAbi.events.CampaignCreated.topic],
     range: { from: params.from },
   })
-  const campaignClosedFilter = logFilter({
-    topic0: [curvePoolBoosterAbi.events.CampaignClosed.topic],
-    range: { from: params.from },
-  })
   const campaignIdUpdatedFilter = logFilter({
     topic0: [curvePoolBoosterAbi.events.CampaignIdUpdated.topic],
     range: { from: params.from },
   })
+  const campaignClosedFilter = logFilter({
+    topic0: [curvePoolBoosterAbi.events.CampaignClosed.topic],
+    range: { from: params.from },
+  })
+
   return defineProcessor({
-    name: `curve-pool-booster-${params.address}`,
+    name: `curve-pool-booster`,
     setup: (processor: EvmBatchProcessor) => {
       processor.addLog(bribeCreatedFilter.value)
     },
     process: async (ctx: Context) => {
+      const campaigns = new Map<string, PoolBoosterCampaign>()
+      const feesCollected = new Map<string, PoolBoosterFeeCollected>()
+
       for (const block of ctx.blocksWithContent) {
         for (const log of block.logs) {
           if (campaignCreatedFilter.matches(log)) {
             const data = curvePoolBoosterAbi.events.CampaignCreated.decode(log)
-            console.log(data)
-          }
-          if (campaignClosedFilter.matches(log)) {
-            const data = curvePoolBoosterAbi.events.CampaignClosed.decode(log)
-            console.log(data)
+            const id = `${ctx.chain.id}-${log.address}`
+            const campaign = new PoolBoosterCampaign({
+              id,
+              chainId: ctx.chain.id,
+              address: log.address,
+              gauge: data.gauge,
+              rewardToken: data.rewardToken,
+              maxRewardPerVote: data.maxRewardPerVote,
+              totalRewardAmount: data.totalRewardAmount,
+            })
+            campaigns.set(id, campaign)
           }
           if (campaignIdUpdatedFilter.matches(log)) {
             const data = curvePoolBoosterAbi.events.CampaignIdUpdated.decode(log)
-            console.log(data)
+            const id = `${ctx.chain.id}-${log.address}`
+            let campaign = campaigns.get(id) || (await ctx.store.get(PoolBoosterCampaign, id))
+            if (campaign) {
+              campaign.campaignId = data.newId
+            }
+          }
+          if (campaignClosedFilter.matches(log)) {
+            const id = `${ctx.chain.id}-${log.address}`
+            let campaign = campaigns.get(id) || (await ctx.store.get(PoolBoosterCampaign, id))
+            if (campaign) {
+              campaign.closed = true
+            }
           }
           if (bribeCreatedFilter.matches(log)) {
             const data = curvePoolBoosterAbi.events.FeeCollected.decode(log)
-            console.log(data)
+            const feeCollected = new PoolBoosterFeeCollected({
+              id: `${ctx.chain.id}-${log.id}`,
+              chainId: ctx.chain.id,
+              address: log.address,
+              feeCollector: data.feeCollector,
+              feeAmount: data.feeAmount,
+              timestamp: new Date(block.header.timestamp),
+              blockNumber: block.header.height,
+              txHash: log.transactionHash,
+            })
+            feesCollected.set(feeCollected.id, feeCollected)
           }
         }
       }
-      await frequencyUpdate(ctx, async (ctx: Context, block: Block) => {
-        const curvePoolBooster = new curvePoolBoosterAbi.Contract(ctx, block.header, params.address)
-        const feeCollector = await curvePoolBooster.feeCollector()
-        console.log(feeCollector)
-      })
+      await ctx.store.insert([...campaigns.values()])
+      await ctx.store.insert([...feesCollected.values()])
     },
   })
 }
