7cb604228ce21ccd3903a168b640f57e4df455e3
Chris Jacobs <apexearth@gmail.com>
2025-02-26 07:52:27 -0800

    fix crash

--- src/templates/otoken/curve-pool-booster.ts
+++ src/templates/otoken/curve-pool-booster.ts
@@ -61,74 +61,78 @@ export const createCurvePoolBoosterProcessor = (params: { from: number }) => {
 
       for (const block of ctx.blocksWithContent) {
         for (const log of block.logs) {
-          // TODO: Validate campaign feeCollector is in our whitelist.
-          if (campaignCreatedFilter.matches(log)) {
-            const data = curvePoolBoosterAbi.events.CampaignCreated.decode(log)
-            const id = `${ctx.chain.id}-${log.address}`
-            const campaign = new PoolBoosterCampaign({
-              id,
-              chainId: ctx.chain.id,
-              address: log.address,
-              gauge: data.gauge,
-              rewardToken: data.rewardToken,
-              maxRewardPerVote: data.maxRewardPerVote,
-              totalRewardAmount: data.totalRewardAmount,
-              closed: false,
-            })
-            campaigns.set(id, campaign)
-          } else if (campaignIdUpdatedFilter.matches(log)) {
-            const data = curvePoolBoosterAbi.events.CampaignIdUpdated.decode(log)
-            const campaign = await getCampaign(log.address)
-            if (campaign) {
-              campaign.campaignId = data.newId
-            }
-          } else if (campaignClosedFilter.matches(log)) {
-            const campaign = await getCampaign(log.address)
-            if (campaign) {
-              campaign.closed = true
-            }
-          } else if (rewardPerVoteUpdatedFilter.matches(log)) {
-            const data = curvePoolBoosterAbi.events.RewardPerVoteUpdated.decode(log)
-            const campaign = await getCampaign(log.address)
-            if (campaign) {
-              campaign.maxRewardPerVote = data.newMaxRewardPerVote
-            }
-          } else if (totalRewardAmountUpdatedFilter.matches(log)) {
-            const data = curvePoolBoosterAbi.events.TotalRewardAmountUpdated.decode(log)
-            const campaign = await getCampaign(log.address)
-            if (campaign) {
-              campaign.totalRewardAmount += data.extraTotalRewardAmount
-            }
-          } else if (feeCollectedFilter.matches(log)) {
-            const campaign = await getCampaign(log.address)
-            if (campaign) {
-              const data = curvePoolBoosterAbi.events.FeeCollected.decode(log)
-              const feeCollected = new PoolBoosterFeeCollected({
+          try {
+            // TODO: Validate campaign feeCollector is in our whitelist.
+            if (campaignCreatedFilter.matches(log)) {
+              const data = curvePoolBoosterAbi.events.CampaignCreated.decode(log)
+              const id = `${ctx.chain.id}-${log.address}`
+              const campaign = new PoolBoosterCampaign({
+                id,
+                chainId: ctx.chain.id,
+                address: log.address,
+                gauge: data.gauge,
+                rewardToken: data.rewardToken,
+                maxRewardPerVote: data.maxRewardPerVote,
+                totalRewardAmount: data.totalRewardAmount,
+                closed: false,
+              })
+              campaigns.set(id, campaign)
+            } else if (campaignIdUpdatedFilter.matches(log)) {
+              const data = curvePoolBoosterAbi.events.CampaignIdUpdated.decode(log)
+              const campaign = await getCampaign(log.address)
+              if (campaign) {
+                campaign.campaignId = data.newId
+              }
+            } else if (campaignClosedFilter.matches(log)) {
+              const campaign = await getCampaign(log.address)
+              if (campaign) {
+                campaign.closed = true
+              }
+            } else if (rewardPerVoteUpdatedFilter.matches(log)) {
+              const data = curvePoolBoosterAbi.events.RewardPerVoteUpdated.decode(log)
+              const campaign = await getCampaign(log.address)
+              if (campaign) {
+                campaign.maxRewardPerVote = data.newMaxRewardPerVote
+              }
+            } else if (totalRewardAmountUpdatedFilter.matches(log)) {
+              const data = curvePoolBoosterAbi.events.TotalRewardAmountUpdated.decode(log)
+              const campaign = await getCampaign(log.address)
+              if (campaign) {
+                campaign.totalRewardAmount += data.extraTotalRewardAmount
+              }
+            } else if (feeCollectedFilter.matches(log)) {
+              const campaign = await getCampaign(log.address)
+              if (campaign) {
+                const data = curvePoolBoosterAbi.events.FeeCollected.decode(log)
+                const feeCollected = new PoolBoosterFeeCollected({
+                  id: `${ctx.chain.id}-${log.id}`,
+                  chainId: ctx.chain.id,
+                  address: log.address,
+                  feeCollector: data.feeCollector,
+                  feeAmount: data.feeAmount,
+                  timestamp: new Date(block.header.timestamp),
+                  blockNumber: block.header.height,
+                  txHash: log.transactionHash,
+                })
+                feesCollected.set(feeCollected.id, feeCollected)
+              }
+            } else if (tokensRescuedFilter.matches(log)) {
+              const data = curvePoolBoosterAbi.events.TokensRescued.decode(log)
+              const tokensRescuedEvent = new PoolBoosterTokensRescued({
                 id: `${ctx.chain.id}-${log.id}`,
                 chainId: ctx.chain.id,
                 address: log.address,
-                feeCollector: data.feeCollector,
-                feeAmount: data.feeAmount,
+                token: data.token,
+                amount: data.amount,
+                receiver: data.receiver,
                 timestamp: new Date(block.header.timestamp),
                 blockNumber: block.header.height,
                 txHash: log.transactionHash,
               })
-              feesCollected.set(feeCollected.id, feeCollected)
+              tokensRescued.set(tokensRescuedEvent.id, tokensRescuedEvent)
             }
-          } else if (tokensRescuedFilter.matches(log)) {
-            const data = curvePoolBoosterAbi.events.TokensRescued.decode(log)
-            const tokensRescuedEvent = new PoolBoosterTokensRescued({
-              id: `${ctx.chain.id}-${log.id}`,
-              chainId: ctx.chain.id,
-              address: log.address,
-              token: data.token,
-              amount: data.amount,
-              receiver: data.receiver,
-              timestamp: new Date(block.header.timestamp),
-              blockNumber: block.header.height,
-              txHash: log.transactionHash,
-            })
-            tokensRescued.set(tokensRescuedEvent.id, tokensRescuedEvent)
+          } catch (err) {
+            console.error(err)
           }
         }
       }
