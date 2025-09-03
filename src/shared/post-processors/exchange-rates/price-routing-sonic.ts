e0310348ed3fa05cd5a4324b25698b414979fe3f
Chris Jacobs <apexearth@gmail.com>
2025-09-03 12:53:18 -0700

    add heights to sonic price routing and utilize chainlink OS feeds

--- src/shared/post-processors/exchange-rates/price-routing-sonic.ts
+++ src/shared/post-processors/exchange-rates/price-routing-sonic.ts
@@ -19,15 +19,46 @@ const createChainlinkPriceFeed = (address: string, decimals: bigint, from: numbe
   }
 }
 
-const chainlinkPriceFeeds: Record<string, (ctx: Context, height: number) => Promise<bigint>> = {
-  ETH_USD: createChainlinkPriceFeed('0x824364077993847f71293B24ccA8567c00c2de11', 8n, 3394229),
-  S_ETH: async (ctx, height) => {
-    const sUsd = await chainlinkPriceFeeds.S_USD(ctx, height)
-    const ethUsd = await chainlinkPriceFeeds.ETH_USD(ctx, height)
-    return (sUsd * PRECISION) / ethUsd
-  },
-  S_USD: createChainlinkPriceFeed('0xc76dfb89ff298145b417d221b2c747d84952e01d', 8n, 4189824),
-}
+const chainlinkPriceFeeds: Record<string, { height: number; get: (ctx: Context, height: number) => Promise<bigint> }> =
+  {
+    ETH_USD: {
+      height: 3394366,
+      get: createChainlinkPriceFeed('0x824364077993847f71293B24ccA8567c00c2de11', 8n, 3394229),
+    },
+    S_ETH: {
+      height: 4189824,
+      get: async (ctx, height) => {
+        const sUsd = await chainlinkPriceFeeds.S_USD.get(ctx, height)
+        const ethUsd = await chainlinkPriceFeeds.ETH_USD.get(ctx, height)
+        return (sUsd * PRECISION) / ethUsd
+      },
+    },
+    S_USD: {
+      height: 4189824,
+      get: createChainlinkPriceFeed('0xc76dfb89ff298145b417d221b2c747d84952e01d', 8n, 4189824),
+    },
+    OS_S: {
+      height: 21623950,
+      get: createChainlinkPriceFeed('0x30caC44b395eB969C9CA0d44dF39e6E0aE8f8D94', 18n, 4189824),
+    },
+    OS_ETH: {
+      height: 21623950,
+      get: async (ctx, height) => {
+        const osS = await chainlinkPriceFeeds.OS_S.get(ctx, height)
+        const sEth = await chainlinkPriceFeeds.S_ETH.get(ctx, height)
+        return (osS * sEth) / PRECISION
+      },
+    },
+    OS_USD: {
+      height: 21623950,
+      get: async (ctx, height) => {
+        const osS = await chainlinkPriceFeeds.OS_S.get(ctx, height)
+        const sEth = await chainlinkPriceFeeds.S_ETH.get(ctx, height)
+        const ethUsd = await chainlinkPriceFeeds.ETH_USD.get(ctx, height)
+        return (((osS * sEth) / PRECISION) * ethUsd) / PRECISION
+      },
+    },
+  }
 
 export const sonicCurrenciesByAddress = invertMap(sonicAddresses.tokens)
 
@@ -41,8 +72,8 @@ export const getSonicPrice = async (ctx: Context, height: number, base: SonicCur
     quote = translateSonicSymbol(quote)
     if (base === quote) return [1_000_000_000_000_000_000n, 18] as const
     const feed = chainlinkPriceFeeds[`${base}_${quote}`]
-    if (feed) {
-      return [await feed(ctx, height), 18] as const
+    if (feed && feed.height <= height) {
+      return [await feed.get(ctx, height), 18] as const
     }
     return undefined
   } catch (err) {
@@ -55,7 +86,6 @@ export const translateSonicSymbol = (symbol: SonicCurrency): SonicCurrencySymbol
   symbol = sonicCurrenciesByAddress[symbol as SonicCurrencyAddress] || symbol
   if (symbol === 'USDC') return 'USD'
   if (symbol === 'wS') return 'S'
-  if (symbol === 'OS') return 'S'
   if (symbol === 'WETH') return 'ETH'
   return symbol
 }
