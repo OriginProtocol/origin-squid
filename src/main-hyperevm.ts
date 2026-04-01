12da8bfe12ee9906404e4828e600e12ffee3f876
Christopher Jacobs <apexearth@gmail.com>
2026-04-01 13:30:24 -0700

    update squid-utils with hyperevm

--- src/main-hyperevm.ts
+++ src/main-hyperevm.ts
@@ -1,23 +1,10 @@
 import 'tsconfig-paths/register'
-
-import { chainConfigs, defineSquidProcessor, run } from '@originprotocol/squid-utils'
-import { processStatus } from '@templates/processor-status'
-import { createMorphoVaultApyProcessor } from '@templates/morpho/processor'
-import { DEFAULT_FIELDS } from '@utils/batch-proccesor-fields'
 import { hyperEvm } from 'viem/chains'
 
-/**
- * Inject HyperEVM into chainConfigs so that defineSquidProcessor can use it.
- * This is necessary because @originprotocol/squid-utils only has mainnet, base,
- * arbitrum, sonic, etc. hardcoded. HyperEVM support will be added upstream.
- */
-Object.assign(chainConfigs, {
-  [hyperEvm.id]: {
-    chain: hyperEvm,
-    gateway: 'https://v2.archive.subsquid.io/network/hyperliquid-mainnet',
-    endpoints: [process.env.RPC_HYPEREVM_ENDPOINT ?? 'https://rpc.hyperliquid.xyz/evm'],
-  },
-})
+import { defineSquidProcessor, run } from '@originprotocol/squid-utils'
+import { createMorphoVaultApyProcessor } from '@templates/morpho/processor'
+import { processStatus } from '@templates/processor-status'
+import { DEFAULT_FIELDS } from '@utils/batch-proccesor-fields'
 
 const FROM_BLOCK = 31_029_000 // ~7 days before 2026-04-01
 
